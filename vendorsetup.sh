9de8f4690152f875c7fbd0568955fefa64abb000
Jon West <electrikjesus@gmail.com>
2021-11-28 14:02:53 -0500

    Only update foss apps when product config changes

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -34,31 +34,44 @@ function lunch
     aosp_lunch $*
 	
 	if [ -d $T/vendor/foss/ ]; then
+		
+		
+		if [ -f $T/vendor/extra/product.config ]; then
+			product=$(cat $T/vendor/extra/product.config)
+			echo "product config loaded: ${product}"
+		fi
+		
 		#~ cd $rompath/vendor/foss
-		echo "FOSS apps found, updating"
+		echo "FOSS apps found, determining if update is needed "
 		mapfile -t variables < <(grep -oP 'TARGET_ARCH \K[^ ]+' $rompath/out/dumpvars-soong.log)
 		for v in "${variables[@]}"; do
 			printf "varname=%s\n" "$v"
 			wd_arch=$v
+			echo -e ${wd_arch} > $T/vendor/extra/product.config
 		done
-		cd $rompath/vendor/foss
-		if [ "$wd_arch" = "arm" ]; then
-			echo -e "Updating foss apps for ARM"
-			. update.sh 4
-		fi 
-		if [ "$wd_arch" = "arm64" ]; then
-			echo -e "Updating foss apps for ARM64"
-			. update.sh 2
+		if [ "$wd_arch" != "$product" ] || [ ! -d $T/vendor/foss/bin/ ]; then
+			echo "FOSS apps need to be updated. Doing that now "
+			cd $rompath/vendor/foss
+			if [ "$wd_arch" = "arm" ]; then
+				echo -e "Updating foss apps for ARM"
+				. update.sh 4
+			fi 
+			if [ "$wd_arch" = "arm64" ]; then
+				echo -e "Updating foss apps for ARM64"
+				. update.sh 2
+			fi
+			if [ "$wd_arch" = "x86_64" ]; then
+				echo -e "Updating foss apps for x86_64"
+				. update.sh 1
+			fi
+			if [ "$wd_arch" = "x86" ]; then
+				echo -e "Updating foss apps for x86"
+				. update.sh 3
+			fi
+			cd $rompath
+		else
+			echo "FOSS apps update not needed "
 		fi
-		if [ "$wd_arch" = "x86_64" ]; then
-			echo -e "Updating foss apps for x86_64"
-			. update.sh 1
-		fi
-		if [ "$wd_arch" = "x86" ]; then
-			echo -e "Updating foss apps for x86"
-			. update.sh 3
-		fi
-		cd $rompath
 	else
 		echo "No FOSS vendor found"
 	fi
